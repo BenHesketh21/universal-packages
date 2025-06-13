757851917f24f0d2a7f88e63226e2e2ee3b143ac
BenHesketh21 <heskethben0@gmail.com>
2025-06-13 21:57:30 +0100

    lint fixes

--- internal/npm/root.go
+++ internal/npm/root.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -39,13 +40,21 @@ func GetPackageName(ociPath string) (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("opening tarball: %w", err)
 	}
-	defer f.Close()
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Fatalf("unable to close file: %v", err)
+		}
+	}()
 
 	gzReader, err := gzip.NewReader(f)
 	if err != nil {
 		return "", fmt.Errorf("gzip reader: %w", err)
 	}
-	defer gzReader.Close()
+	defer func() {
+		if err := gzReader.Close(); err != nil {
+			log.Fatalf("unable to close file: %v", err)
+		}
+	}()
 
 	tarReader := tar.NewReader(gzReader)
 
@@ -174,7 +183,11 @@ func SavePackageJSON(path string, pkg *PackageJSON) error {
 	if err != nil {
 		return err
 	}
-	defer file.Close()
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Fatalf("unable to close file: %v", err)
+		}
+	}()
 
 	encoder := json.NewEncoder(file)
 	encoder.SetIndent("", "  ")  // pretty print with 2-space indent
